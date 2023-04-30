ec697aa7c015dd6fe1bf8dffcc0a9eb79345d3e4
Er2 <er2@dismail.de>
2023-04-30 16:54:29 +0300

    fix color argument error, lol

--- src/cmds/color.js
+++ src/cmds/color.js
@@ -9,9 +9,9 @@ export default  {
 		}
 	],
 	run: async (C, msg) => {
-		const col = new rgbcolor(msg.args[0].value.toLowerCase())
+		const col = new rgbcolor(msg.args.color.toLowerCase())
 		msg.createEmbed(new Embed()
-			.author((col.ok ? msg.loc.iscol : msg.loc.isntcol) +' '+ msg.args[0].value, C.api.user.dynamicAvatarURL('webp'))
+			.author((col.ok ? msg.loc.iscol : msg.loc.isntcol) +' '+ msg.args.color, C.api.user.dynamicAvatarURL('webp'))
 			.thumbnail(`http://singlecolorimage.com/get/${col.ok ? col.toHex().toString().slice(1) : '2C2F33'}/100x100`)
 			.color(col.ok ? col.toHex() : '2C2F33')
 			.field('HEX', col.ok ? col.toHex() : msg.loc.isntcol)
